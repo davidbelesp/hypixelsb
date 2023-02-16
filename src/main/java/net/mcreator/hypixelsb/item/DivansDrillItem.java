f7db3c2101dadef08976c568f8559f03eb4fbb70
David <dbeltrane01@iesarroyoharnina.es>
2023-02-16 14:03:48 +0100

    Divans Drill

--- src/main/java/net/mcreator/hypixelsb/item/DivansDrillItem.java
+++ src/main/java/net/mcreator/hypixelsb/item/DivansDrillItem.java
@@ -45,7 +45,36 @@ public class DivansDrillItem extends PickaxeItem {
 	@Override
 	public void appendHoverText(ItemStack itemstack, Level world, List<Component> list, TooltipFlag flag) {
 		super.appendHoverText(itemstack, world, list, flag);
+		list.add(Component.literal("§8Breaking Power 10"));
 		list.add(Component.literal(""));
-		list.add(Component.literal("\u00A7b\u00A7lDIVINE DRILL"));
+		list.add(Component.literal("§7Damage: §c+70"));
+		list.add(Component.literal("§7Mining Speed: §a+1,800"));
+		list.add(Component.literal("§7Mining Fortune: §a+150"));
+		list.add(Component.literal(" §8[§7⸕§8] §8[§7⸕§8] §8[§8☘§8] §8[§8☘§8] §8[§8✦§8]"));
+		list.add(Component.literal(""));
+		list.add(Component.literal("§7Fuel Tank: §cNot Installed"));
+		list.add(Component.literal("§7Increases fuel capacity with"));
+		list.add(Component.literal("§7part installed."));
+		list.add(Component.literal(""));
+		list.add(Component.literal("§7Drill Engine: §cNot Installed"));
+		list.add(Component.literal("§7Increases §6⸕ Mining Speed"));
+		list.add(Component.literal("§7with part installed."));
+		list.add(Component.literal(""));
+		list.add(Component.literal("§7Upgrade Module: §cNot Installed"));
+		list.add(Component.literal("§7Applies a passive upgrade with"));
+		list.add(Component.literal("§7part installed."));
+		list.add(Component.literal(""));
+		list.add(Component.literal("§7Apply Drill Parts to this Drill"));
+		list.add(Component.literal("§7by talking to a §2Drill"));
+		list.add(Component.literal("§7Mechanic§7!"));
+		list.add(Component.literal(""));
+		list.add(Component.literal("§7Fuel: §23,000§8"));
+		list.add(Component.literal("3k"));
+		list.add(Component.literal(""));
+		list.add(Component.literal("§8This item can be reforged!"));
+		list.add(Component.literal("§4❣ §cRequires §5Heart of the"));
+		list.add(Component.literal("§7Mountain Tier 7."));
+		list.add(Component.literal("§b§lDIVINE DRILL"));
+
 	}
 }
